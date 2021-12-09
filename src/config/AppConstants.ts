026bd42bc80340e4b5e1b29d5ccd9ce9503cf13a
Jan Adamczyk <j_adamczyk@hotmail.com>
2021-12-09 02:04:58 +0100

    cleanup

--- src/config/AppConstants.ts
+++ src/config/AppConstants.ts
@@ -7,13 +7,6 @@ export const APP_CAMPAIGN_REQUEST = (campaignAddress: string) => `${APP_CAMPAIGN
 export const APP_CAMPAIGN_REQUEST_CREATE = (campaignAddress: string) => `${APP_CAMPAIGN_REQUEST(campaignAddress)}/create`
 export const APP_CAMPAIGN_REQUEST_APPROVE = (campaignAddress: string) => `${APP_CAMPAIGN_REQUEST(campaignAddress)}/approve`
 export const APP_CAMPAIGN_REQUEST_FINALIZE = (campaignAddress: string) => `${APP_CAMPAIGN_REQUEST(campaignAddress)}/finalize`
-export const APP_PATH_ARTICLES = "/articles"
-export const APP_PATH_STOCKS = "/stocks"
-export const APP_PATH_CALCULATOR = "/calculator"
-export const APP_PATH_SUPPORT_US = "/support-us"
-export const APP_PATH_COMMUNITY = "/community"
 export const FORBIDDEN_URL = "/forbidden"
-
-export const APP_LOCALSTORAGE_ID = "ID"
 
 export const IS_DARK_MODE = 'darkMode';
